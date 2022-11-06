467986fe0a9525ec04a4ec82f6b0005d79474a6a
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-11-06 14:20:16 +0700

    test: add tests for idempotent resolve and reject

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -14,4 +14,38 @@ describe('Future', () => {
         done()
       })
   })
+
+  it('should reject future', done => {
+    const onRejected = jest.fn()
+    FutureFactory((_, reject) => reject('peanut butter'))
+      .then(null, onRejected)
+      .then(() => {
+        expect(onRejected).toHaveBeenCalledTimes(1)
+        done()
+      })
+  })
+
+  it('should be idempotent for resolved state', done => {
+    const onFulfilled = jest.fn()
+    const onRejected = jest.fn()
+    FutureFactory(resolve => resolve('peanut butter'))
+      .then(onFulfilled, onRejected)
+      .then(() => {
+        expect(onRejected).not.toHaveBeenCalled()
+        expect(onFulfilled).toHaveBeenCalledTimes(1)
+        done()
+      })
+  })
+
+  it('should be idempotent for rejected state', done => {
+    const onFulfilled = jest.fn()
+    const onRejected = jest.fn()
+    FutureFactory((_, reject) => reject('peanut butter'))
+      .then(onFulfilled, onRejected)
+      .then(() => {
+        expect(onFulfilled).not.toHaveBeenCalled()
+        expect(onRejected).toHaveBeenCalledTimes(1)
+        done()
+      })
+  })
 })
